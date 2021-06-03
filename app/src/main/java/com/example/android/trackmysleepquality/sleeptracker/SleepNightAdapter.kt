06102f44fc537255eb5f9818db1e5c344c6c74d2
altin <altincampbell121690@gmail.com>
2021-06-02 21:37:23 -0400

    changed out view holder for custom view holder and binded views using the new viewholder

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -4,40 +4,52 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter:RecyclerView.Adapter<TextItemViewHolder>() {
+class SleepNightAdapter:RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     var dataList = listOf<SleepNight>()
     set(value){//custom setter to data variable alerts recycler view that data has chanced
         field = value
         notifyDataSetChanged()
     } // i dont think this is best practice
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view,parent,false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_sleep_night,parent,false)
+        return ViewHolder(view)
     }
-// lets the recylcer view know how to draw the item
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var item = dataList[position]
-    if (item.sleepQuality <= 1) {
-        holder.textView.setTextColor(Color.RED)
-    } else {
-        holder.textView.setTextColor(Color.BLACK)
+        val resources = holder.itemView.resources
+        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli,item.endTimeMilli, resources)
+        holder.quality.text = convertNumericQualityToString(item.sleepQuality,resources)
+        holder.qualityImage.setImageResource(
+            when(item.sleepQuality){
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            }
+        )
     }
-        holder.textView.text = dataList[position].sleepQuality.toString()
-
-    }
-
     // lets recycler know how many items there are
     override fun getItemCount() = dataList.size
 
     class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
-        val sleepQuality: TextView = itemView.findViewById(R.id.sleep_length)
+        val sleepLength:TextView = itemView.findViewById(R.id.sleep_length)
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
         val quality : TextView = itemView.findViewById(R.id.quality_string)
     }
+
+
+
 }
